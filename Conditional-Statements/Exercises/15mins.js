fddb07c1d073777615b33d8231bff00070945fd5
Georgi Nikov <nikov.george@gmail.com>
2022-09-23 18:30:58 +0300

    Create 15mins.js

---
+++ Conditional-Statements/Exercises/15mins.js
@@ -0,0 +1,22 @@
+function time (input){
+    let hours = Number(input[0]);
+    let minutes = Number(input[1]);
+    minutes =minutes + 15;
+
+    if (minutes >= 60){
+    minutes=minutes-60
+    hours= hours + 1;
+    }
+    if (hours >=24){
+        hours= 0;
+    }
+    if (minutes < 10){
+    console.log(`${hours}:0${minutes}`);
+
+    }  else {
+        console.log(`${hours}:${minutes}`);
+    }
+
+
+}
+time(["1", "46"])

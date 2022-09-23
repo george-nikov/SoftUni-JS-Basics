ce4f0bac3db4df3a43b0e2d608072fa04fe31a96
Georgi Nikov <nikov.george@gmail.com>
2022-09-23 18:32:54 +0300

    Create sum.js

---
+++ Conditional-Statements/Exercises/sum.js
@@ -0,0 +1,18 @@
+function secs (input){
+    let a = Number(input[0]);
+    let b = Number(input[1]);
+    let c = Number(input[2]);
+    
+    let totalTime = a + b + c;
+    let minutes = Math.floor(totalTime /60);
+    let seconds = totalTime % 60;
+
+    if (seconds <10){
+        console.log(`${minutes}:0${seconds}`);
+    }   else {
+        console.log(`${minutes}:${seconds}`);
+    }
+
+}
+
+secs(["35","45","44"])

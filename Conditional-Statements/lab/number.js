e0194541da4a5465d961443a50aa49091d6394b5
Georgi Nikov <nikov.george@gmail.com>
2022-09-23 18:28:53 +0300

    Create number.js

---
+++ Conditional-Statements/lab/number.js
@@ -0,0 +1,13 @@
+function number (input){
+    let a = Number(input[0]);
+    let b = Number(input[1]);
+
+    if (a > b){
+        console.log(a);
+    }   else{
+        console.log(b);
+    }
+
+}
+
+number(["5", "3"])

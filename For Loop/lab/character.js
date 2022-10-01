639de640618423e099270fb400aec7a754954158
Georgi Nikov <nikov.george@gmail.com>
2022-10-01 23:33:43 +0300

    Create character.js

---
+++ For Loop/lab/character.js
@@ -0,0 +1,12 @@
+function character (input){
+
+    let name = input[0];
+
+    for (let i = 0 ; i < name.length ; i++) {
+        let letter = name [i];
+        console.log(letter);
+
+    }
+
+}
+character(['softuni'])

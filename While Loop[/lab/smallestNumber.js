75872ecaf3624bb64634abab9f6591d268540e94
Georgi Nikov <nikov.george@gmail.com>
2022-10-11 11:24:32 +0300

    Create smallestNumber.js

---
+++ While Loop[/lab/smallestNumber.js
@@ -0,0 +1,20 @@
+function smallestNumber(input) {
+    
+    let inputElement = input[0];
+    let index = 1;
+    let min = Number.MAX_SAFE_INTEGER;
+    while (inputElement !== 'Stop') {
+        let currentNum = Number(inputElement)
+        if (currentNum < min) {
+            min = currentNum;
+        }
+        inputElement = input[index];
+        index++;
+    }
+    console.log(min);
+}
+smallestNumber(["100",
+"99",
+"80",
+"70",
+"Stop"])
